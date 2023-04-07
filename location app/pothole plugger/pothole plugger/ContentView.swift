b278eb5f80cf74d9f40b03df01664fa65b6a3b1c
Max Link <66765685+MaxDLink@users.noreply.github.com>
2023-04-06 22:19:01 -0500

    Add files via upload

---
+++ location app/pothole plugger/pothole plugger/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pothole plugger
+//
+//  Created by Max Link on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ location app/pothole plugger/pothole plugger/pothole_pluggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pothole_pluggerApp.swift
+//  pothole plugger
+//
+//  Created by Max Link on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct pothole_pluggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
